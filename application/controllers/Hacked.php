68b3aeb898b5adb67f6bd23247dc07b683041ee5
Sigit Suryono <sigit_suryono25>
2020-03-21 09:24:36 +0700

    initial commit

---
+++ application/controllers/Hacked.php
@@ -0,0 +1,31 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of Hacked
+ *
+ * @author sigit
+ */
+class Hacked extends CI_Controller {
+
+    function register() {
+        
+    }
+
+    function login() {
+        
+    }
+
+    function update_profile() {
+        
+    }
+
+    function delete_account() {
+        
+    }
+}
---
+++ application/models/Query_m.php
@@ -0,0 +1,38 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of Query_m
+ *
+ * @author sigit
+ */
+class Query_m extends CI_Model {
+
+    //put your code here
+
+    function getDetailByEmail() {
+        
+    }
+
+    function getDetailUser() {
+        
+    }
+
+    function updateAccount() {
+        
+    }
+
+    function registerToDb() {
+        
+    }
+
+    function removeAccountFromDb() {
+        
+    }
+
+}
